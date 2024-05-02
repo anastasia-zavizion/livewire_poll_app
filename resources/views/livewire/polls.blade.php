eb5764c82e89ec0793d5793ccdda50cacc90bf39
Nastya Zavizion <nastya.gorobets95@gmail.com>
2024-05-02 22:06:59 +0300

    Votes logic

--- resources/views/livewire/polls.blade.php
+++ resources/views/livewire/polls.blade.php
@@ -4,7 +4,7 @@
           <h3>{{$poll->title}}</h3>
           @foreach($poll->options as $option)
               <div class="mb-2">
-                  <button class="btn">Vote</button> {{$option->name}} ({{$option->votes->count()}})
+                  <button wire:click="vote({{$option->id}})" class="btn">Vote</button> {{$option->name}} ({{$option->votes->count()}})
               </div>
           @endforeach
       </div>
